3a09f9df49ad6a9f33e69130b91621a034c7c060
ColonelBlack94 <colonelblack94@gmail.com>
2014-02-03 11:40:15 +0100

    UIDaten ausgelagert in eigene Klasse

--- Classes/UI.cpp
+++ Classes/UI.cpp
@@ -1,41 +1,46 @@
 #include "UI.h"
 
-UI::UI(Node* _pSrcNode, char* _uiFileLoc)
+UI::UI(Node* _pSrcNode, char* _uiLocation)
 {
+	m_pData = nullptr;
 	m_pNode = _pSrcNode;
-	m_uiFileLoc = _uiFileLoc;
 	m_enabled = false;
-	m_pEngine = nullptr;
-	m_pUINode = nullptr;
+	m_uiLoc = _uiLocation;
 }
 
 UI::~UI()
 {
-
+	freeResources();
 }
 
 void UI::freeResources()
 {
-	if (m_pEngine == nullptr) return;
+	if (m_pData == nullptr) return;
 
 	//Node aufraeumen
-	m_pUINode->removeFromParentAndCleanup(true);
-	m_pUINode->release();
-	m_pUINode = nullptr;
+	m_pData->pUINode->removeFromParentAndCleanup(true);
+	m_pData->pUINode->release();
+	m_pData->pUINode = nullptr;
 
 	//Engine aufraeumen
 	//delete m_pEngine;
-	m_pEngine = nullptr;
+
+	delete m_pData;
+	m_pData = nullptr;
 }
 
 void UI::loadResources()
 {
-	if (m_pEngine != nullptr) return;
+	if (m_pData != nullptr) return;
+
+	m_pData = new UIData(m_uiLoc);
 
 	//Node generieren
-	m_pUINode = Node::create();
-	m_pEngine = ScriptEngineManager::getInstance()->getScriptEngine();
-	//m_pEngine->executeScriptFile(m_uiFileLoc);
+	m_pData->pUINode = Node::create();
+	//Engine holen
+	m_pData->pEngine = ScriptEngineManager::getInstance()->getScriptEngine();
+	//Script ausfuehren
+	m_pData->pEngine->executeScriptFile((const char*)(m_pData->scriptLoc));
 }
 
 void UI::reloadUI()
@@ -48,17 +53,45 @@ void UI::reloadUI()
 void UI::enableUI()
 {
 	if (m_enabled) return;
-	if (m_pEngine == nullptr) loadResources();
+	if (m_pData == nullptr) loadResources();
 	
-	m_pNode->addChild(m_pUINode);
+	m_pNode->addChild(m_pData->pUINode);
 	m_enabled = true;
 }
 
 void UI::disableUI()
 {
 	if (!m_enabled) return;
-	if (m_pEngine == nullptr) return;
+	if (m_pData == nullptr) return;
 
-	m_pUINode->removeFromParent();
+	m_pData->pUINode->removeFromParent();
 	m_enabled = false;
+}
+
+UIData::UIData(char* _uiLocation)
+{
+	uiLoc = new std::string(_uiLocation);
+
+	//Position der pList
+	listLoc = new std::string(_uiLocation);
+	listLoc->append("/atlas.plist");
+
+	//Position des Scripts
+	scriptLoc = new std::string(_uiLocation);
+	scriptLoc->append("/ui.lua");
+
+	//Nicht verwaltete Variablen nullen
+	pUINode = nullptr;
+	pEngine = nullptr;
+}
+
+UIData::~UIData()
+{
+	delete uiLoc;
+	delete listLoc;
+	delete scriptLoc;
+
+	//Nicht verwaltete Variablen nullen
+	pUINode = nullptr;
+	pEngine = nullptr;
 }
--- Classes/UI.h
+++ Classes/UI.h
@@ -2,13 +2,26 @@
 #define __UI_H__
 
 #include "cocos2d.h"
-
 USING_NS_CC;
+
+class UIData
+{
+public:
+	UIData(char* _uiLocation);
+	~UIData(void);
+
+	std::string* uiLoc;
+	std::string* listLoc;
+	std::string* scriptLoc;
+
+	Node* pUINode;
+	ScriptEngineProtocol* pEngine;
+};
 
 class UI
 {
 public:
-	UI(Node* _pSrcNode, char* _uiFileLocation);
+	UI(Node* _pSrcNode, char* _uiLocation);
 	virtual ~UI(void);
 
 	void freeResources();
@@ -17,13 +30,11 @@ public:
 	void disableUI();
 	bool isEnabled() { return m_enabled; }
 private:
-	Node* m_pUINode;
+	char* m_uiLoc;
 	Node* m_pNode;
-	char* m_uiFileLoc;
 	bool m_enabled;
-	ScriptEngineProtocol* m_pEngine;
+	UIData* m_pData;
 
 	void loadResources();
 };
-
 #endif __UI_H__
