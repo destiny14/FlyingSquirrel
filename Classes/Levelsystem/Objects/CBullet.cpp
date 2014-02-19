5acde867d21ad46165efd1f84f9c96d3d86f9c1c
Engfii <sebastian.engfer@live.de>
2014-02-19 11:17:03 +0100

    bullet shoot - player can shoot

--- Classes/Levelsystem/Objects/CBullet.cpp
+++ Classes/Levelsystem/Objects/CBullet.cpp
@@ -4,23 +4,20 @@
 #include "Bullet.h"
 #include "..\Components\Collider.h"
 
-Bullet* Bullet::createNut(MainLayer* parent, Point* position, Point* direction, float force)
+Bullet* Bullet::createNut(MainLayer* parent, Point position, float direction, float force)
 {
 	Bullet* bullet = new Bullet();
 	Texture* tex = Texture::create("nuss.png");
+	tex->retain();
 
 	bullet->setTexture(tex);
 	bullet->setCollider();
 	bullet->setParent(parent);
 	bullet->setGround(false);
-	bullet->setPosition(position->x, position->y);
+	bullet->setPosition(position.x + (150.0f * direction), position.y);
+	bullet->setAffectedByGravity(true);
 
-	m_pDirection = direction;
-	m_force = force;
-
-	m_bullettype = Nut;
-
-	bullet->init();
+	bullet->init(direction, force);
 
 	return bullet;
 }
@@ -29,14 +26,19 @@ Bullet::Bullet() {}
 
 Bullet::~Bullet() {}
 
-bool Bullet::init()
+bool Bullet::init(float direction, float force)
 {
+	m_direction = direction;
+	m_force = force;
+
+	m_bullettype = Nut;
+
 	return true;
 }
 
 void Bullet::update(float dt)
 {
-
+	this->setPositionX(this->getPosition().x + (m_force * m_direction));
 }
 
 void Bullet::setCollider()
